2a09ea7767f8c73d8c81c06058586d59f6612a11
Joe Kramer <jjk2145@g.rit.edu>
2016-03-18 00:19:18 -0400

    code4

--- src/controllers/Account.js
+++ src/controllers/Account.js
@@ -15,11 +15,44 @@ var logout = function(req, res){
 };
 
 var login = function(req, res){
+	if(!req.body.username || !req.body.pass){
+		return res.status(400).json({error: "RAWR! All feilds required"});
+	}
 	
+	Accoutn.AccountModel.authenticate(req.body.username, req.body.pass, function(err,account){
+		if(err || !account){
+			return res.status(401).json({error: "Wrong username or password"});
+		}
+		res.json({redirect: '/maker'});
+	});
 };
 
 var signup = function(req, res){
+	if(!req.body.username || !req.body.pass || !reqbody.pass2) {
+		return res.status(400).json({error: "RAWR! All fields are required"});
+	}
 	
+	if(req.body.pass !== req.body.pass2) {
+		return res.status(400).json({error: "RAWR! passwords do not match"});
+	}
+	Account.AccountModel.generateHash(req.body.pass, function(salt, hash){
+		var accountData = {
+			username: req.body.username,
+			salt: salt,
+			password: hash
+		};
+		
+		var newAccount = new Account.AccountModel(accountData);
+		
+		newAccount.save(function(err){
+			if(err){
+				console.log(err);
+				return res.status(400).json({error: 'An error occured'});
+			}
+			
+			res.json({redirect: '/maker'});
+		});
+	});
 };
 
 module.exports.loginPage = loginPage;
